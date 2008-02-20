91fb08e1381ae477f88f0c1976a8c817f418961d
janb.webtide <janb.webtide@d7dbba05-0943-0410-a12d-11b33697fbca>
2008-02-20 03:55:49 +0000

    Workaround for ResourceBundle problem (see android bug reports 173, 250).

--- src/org/mortbay/ijetty/R.java
+++ src/org/mortbay/ijetty/R.java
@@ -15,20 +15,26 @@ public final class R {
         public static final int jicon=0x7f020001;
     }
     public static final class id {
-        public static final int start=0x7f050000;
-        public static final int stop=0x7f050001;
+        public static final int start=0x7f060000;
+        public static final int stop=0x7f060001;
     }
     public static final class layout {
         public static final int jetty_controller=0x7f030000;
         public static final int main=0x7f030001;
     }
+    public static final class raw {
+        public static final int javax_servlet_http_localstrings=0x7f040000;
+        public static final int javax_servlet_localstrings=0x7f040001;
+        public static final int org_mortbay_jetty_encoding=0x7f040002;
+        public static final int org_mortbay_jetty_mime=0x7f040003;
+    }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int jetty_controller=0x7f040004;
-        public static final int jetty_started=0x7f040001;
-        public static final int jetty_stopped=0x7f040002;
-        public static final int manage_jetty=0x7f040003;
-        public static final int start_jetty=0x7f040005;
-        public static final int stop_jetty=0x7f040006;
+        public static final int app_name=0x7f050000;
+        public static final int jetty_controller=0x7f050004;
+        public static final int jetty_started=0x7f050001;
+        public static final int jetty_stopped=0x7f050002;
+        public static final int manage_jetty=0x7f050003;
+        public static final int start_jetty=0x7f050005;
+        public static final int stop_jetty=0x7f050006;
     }
 }
