82ae4cb1e6cafd4717ff1ff3119be84a4f594a14
janb.webtide <janb.webtide@d7dbba05-0943-0410-a12d-11b33697fbca>
2008-02-20 04:30:38 +0000

    Changing jetty icon for android.

--- src/org/mortbay/ijetty/R.java
+++ src/org/mortbay/ijetty/R.java
@@ -12,7 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int jicon=0x7f020001;
+        public static final int jetty=0x7f020001;
+        public static final int jicon=0x7f020002;
     }
     public static final class id {
         public static final int start=0x7f060000;
