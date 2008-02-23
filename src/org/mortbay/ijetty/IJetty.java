2a12f92ef2738b226935760fd1facba3669928e4
janb.webtide <janb.webtide@d7dbba05-0943-0410-a12d-11b33697fbca>
2008-02-23 05:29:47 +0000

    Change app to show data from device

--- src/org/mortbay/ijetty/IJetty.java
+++ src/org/mortbay/ijetty/IJetty.java
@@ -1,12 +1,18 @@
 package org.mortbay.ijetty;
 
 
+import java.util.Iterator;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class IJetty extends Activity {
     /** Called when the activity is first created. */
@@ -14,32 +20,60 @@ public class IJetty extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.jetty_controller);
-
+       
         // Watch for button clicks.
-        Button button = (Button)findViewById(R.id.start);
-        button.setOnClickListener(
+        final Button startButton = (Button)findViewById(R.id.start);
+        startButton.setOnClickListener(
                 new OnClickListener()
                 {
                     public void onClick(View v)
-                    {
-                        // Make sure the service is started.  It will continue running
-                        // until someone calls stopService().
-                        startService(new Intent(IJetty.this,
-                                IJettyService.class), null);
+                    {  
+                        SharedPreferences prefs = getSharedPreferences("jetty", MODE_WORLD_WRITEABLE);
+                        if (prefs!=null)
+                        {
+                            try
+                            {
+                                Map m = prefs.getAll();
+                                Iterator itor = m.entrySet().iterator();
+                                while (itor.hasNext())
+                                {
+                                    Map.Entry me = (Map.Entry)itor.next();
+                                    Log.i("Jetty", "Map entry "+me.getKey()+":"+me.getValue());
+                                }
+                            }
+                            catch (NullPointerException e)
+                            {
+                                Log.e("Jetty", "No preferences");
+                            }
+
+                        }
+                        else
+                            Log.i("Jetty", "there are no preferences");
+                        //boolean isRunning = prefs.getBoolean("isRunning", false);
+                        //Log.i("Jetty", "is running: "+isRunning);
+                        //if (isRunning)
+                        //    Toast.makeText(IJetty.this, getText(R.string.jetty_started), Toast.LENGTH_SHORT).show();
+                        //else
+                            startService(new Intent(IJetty.this, IJettyService.class), null);
+
                     }
                 }
         );
-        button = (Button)findViewById(R.id.stop);
-        button.setOnClickListener(
+        
+        Button stopButton = (Button)findViewById(R.id.stop);
+        stopButton.setOnClickListener(
                 new OnClickListener()
                 {
                     public void onClick(View v)
                     {
-                        // Cancel a previous call to startService().  Note that the
-                        // service will not actually stop at this point if there are
-                        // still bound clients.
-                        stopService(new Intent(IJetty.this,
-                                IJettyService.class));
+                        //SharedPreferences prefs = getSharedPreferences("jetty", MODE_WORLD_WRITEABLE);
+                        //boolean isRunning = prefs.getBoolean("isRunning", false);
+                        //if (!isRunning)
+                        //    Toast.makeText(IJetty.this, getText(R.string.jetty_stopped), Toast.LENGTH_SHORT).show();
+                        //else
+                        //Log.i("Jetty", "is running: "+isRunning);
+                            stopService(new Intent(IJetty.this, IJettyService.class));
+                        
                     }
                 }
         );
--- src/org/mortbay/ijetty/IJettyService.java
+++ src/org/mortbay/ijetty/IJettyService.java
@@ -2,15 +2,23 @@ package org.mortbay.ijetty;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Map;
 
 
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.Resources;
-import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
 import android.os.IBinder;
+import android.provider.Contacts;
+import android.provider.CallLog;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -23,7 +31,6 @@ import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.HttpConnection;
 import org.mortbay.jetty.Request;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 
@@ -33,9 +40,9 @@ public class IJettyService extends Service
     private NotificationManager mNM;
     private Server server;
     private static Resources __resources;
-    
+    private SharedPreferences preferences;
 
-    public static class HelloHandler extends AbstractHandler
+    public class HelloHandler extends AbstractHandler
     {
         public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException
         {
@@ -44,7 +51,102 @@ public class IJettyService extends Service
             
             response.setContentType("text/html");
             response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<h1>Hello OneHandler</h1>");
+            PrintWriter writer = response.getWriter();
+            ContentResolver resolver = getContentResolver();
+            Map params = request.getParameterMap();
+            boolean wantContacts = params.get("contacts")!=null;
+            boolean wantSystemSettings = params.get("system")!=null;
+            boolean wantCallLog = params.get("calls")!=null;
+            
+            if (params.isEmpty())
+            {
+                writer.println("<h1>Options</h1>");
+                writer.println("<ul>");
+                writer.println("<li><a href='/?contacts'>Contacts</a></li>");
+                writer.println("<li><a href='/?system'>System Settings</a></li>");
+                writer.println("<li><a href='/?calls'>Call Log</a></li>");
+                writer.println("</ul>");
+            }
+            else if (wantContacts)
+              doContacts(resolver, writer);
+            else if (wantSystemSettings)
+                doSystemSettings (resolver, writer);
+            else if (wantCallLog)
+                doCallLog (resolver, writer);
+        }
+        
+        public void doContacts (ContentResolver resolver, PrintWriter writer)
+        {
+            String[] projection = new String[] {
+                    android.provider.BaseColumns._ID,
+                    android.provider.Contacts.PeopleColumns.NAME,
+                    android.provider.Contacts.PhonesColumns.NUMBER,
+                    android.provider.Contacts.PeopleColumns.PHOTO};
+            Cursor cursor = resolver.query(Contacts.People.CONTENT_URI, projection, null, null, null);  
+            if (cursor!=null)
+            {
+                String[] cols = cursor.getColumnNames();
+                formatTable (cols, cursor, writer);
+            }
+        }
+
+        public void doCallLog (ContentResolver resolver, PrintWriter writer)
+        {
+            String[] projection = new String[] 
+                                             {
+                    CallLog.Calls.DATE,
+                    CallLog.Calls.DURATION,
+                    CallLog.Calls.NEW,
+                    CallLog.Calls.NUMBER,
+                    CallLog.Calls.NUMBER_TYPE,
+                    CallLog.Calls.NAME
+                                             };
+            Cursor cursor = resolver.query(CallLog.Calls.CONTENT_URI, projection, null, null, null);
+            writer.println("<h1>Call Log</h1>");
+            String[] cols = cursor.getColumnNames();
+            formatTable(cols, cursor, writer);  
+        }
+
+        public void doSystemSettings (ContentResolver resolver, PrintWriter writer)
+        {  
+            String[] projection = new String[] 
+                                             {
+                    Settings.System._ID,
+                                             };
+            writer.println("<h1>System Settings</h1>");
+            Cursor cursor = resolver.query(Settings.System.CONTENT_URI, null, null, null, null);
+            String[] cols = cursor.getColumnNames();
+            formatTable(cols, cursor, writer);
+        }
+        
+        private void formatTable (String[] colNames, Cursor cursor, PrintWriter writer)
+        {   
+            if (colNames!=null && cursor!=null && writer!=null)
+            {
+                writer.println("<table style='border-width:3px;border-style:groove;border-color:#aaaaaa;'>");
+                writer.println("<tr>");
+                for (int i=0;i<colNames.length;i++)
+                    writer.println("<th style='font-variant:small-caps;align:left;'>"+colNames[i]+"</th>");
+                writer.println("</tr>");
+                int row = 0;
+                while (cursor.next())
+                {  
+                    String style = "";
+                    if (row%2==0)
+                        style = "'background:#ffffff;border:solid #aaaaaa 1px;'";
+                    else
+                        style = "'background:#efefef;border:solid #aaaaaa 1px;'";
+                    
+                    writer.println("<tr>");
+                    for (int i=0;i<colNames.length;i++)
+                            writer.println("<td style="+style+">"+cursor.getString(i)+"</td>");
+
+                    writer.println("</tr>");
+                    ++row;
+                }
+                writer.println("</table>");
+
+            }
         }
     }
 
@@ -88,6 +190,11 @@ public class IJettyService extends Service
             Log.i("Jetty", "Jetty starting");
             org.mortbay.log.Log.setLog(new AndroidLog());
             server.start();
+            preferences = getSharedPreferences("jetty", MODE_WORLD_READABLE);
+            Editor editor = preferences.edit();
+            editor.putBoolean("isRunning", true);
+            editor.commit();
+            
             Log.i("Jetty", "Jetty started");
         }
         catch (Exception e)
@@ -112,9 +219,13 @@ public class IJettyService extends Service
 
                 Log.i("Jetty", "Jetty stopping");
                 server.stop();
-                Log.i("Jetty", "About to do join");
                 server.join();
                 server=null;
+
+                Editor editor = preferences.edit();
+                editor.putBoolean("isRunning", false);
+                editor.commit();
+
                 Log.i("Jetty", "****Jetty stopped");
                 __resources=null;
             }
