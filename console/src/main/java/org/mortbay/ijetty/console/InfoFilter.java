ce6e4a49b300b8f7b76078413d611f41439fbfac
aquabeta <aquabeta@d7dbba05-0943-0410-a12d-11b33697fbca>
2008-06-06 06:23:38 +0000

    Add in the 'console' webapp project. This should build fine. :)

---
+++ console/src/main/java/org/mortbay/ijetty/console/InfoFilter.java
@@ -0,0 +1,70 @@
+//========================================================================
+//$Id$
+//Copyright 2008 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package org.mortbay.ijetty.console;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.util.URIUtil;
+
+public class InfoFilter implements Filter
+{
+
+    public void destroy()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        String servletPath=httpRequest.getServletPath();
+        String pathInfo=httpRequest.getPathInfo();
+        String pathInContext=URIUtil.addPaths(servletPath,pathInfo);
+        boolean endsWithSlash=pathInContext.endsWith(URIUtil.SLASH);
+           
+        
+        if (pathInContext.equals("")||pathInContext.equals("/"))
+        {
+            if (!endsWithSlash)
+                pathInContext+="/";
+            
+            RequestDispatcher dispatcher = httpRequest.getRequestDispatcher(pathInContext+"app");
+            dispatcher.forward(request, response);
+        
+        }
+        else
+            chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException
+    {   
+        
+    }
+
+}
