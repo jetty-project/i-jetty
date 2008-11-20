39cf9dddf9b136b6322400b368db730a9e011e4b
aquabeta <aquabeta@d7dbba05-0943-0410-a12d-11b33697fbca>
2008-11-20 02:43:05 +0000

    * Change usage for create-sdcard script. * Added a build-linux script which helps automate compiling/testing.

--- trunk/scripts/create-sdcard.sh
+++ trunk/scripts/create-sdcard.sh
@@ -3,25 +3,25 @@
 # create-sdcard.sh
 # Creates a new small sdcard.img file, for use as an SD card.
 #
-# Usage: ./create-sdcard.sh [image-size-in-MB]
+# Usage: ./create-sdcard.sh filename.img [image-size-in-MB]
 
 DEFAULT_IMAGE_SIZE_MB=60
 DEFAULT_IMAGE_SIZE=$(($DEFAULT_IMAGE_SIZE_MB * 1024 * 1024))
 
 IMAGE_SIZE=0
 
-if [ -e "sdcard.img" ]; then
-    echo "sdcard.img already exists!"
+if [ -e "$1" ]; then
+    echo "$1 already exists!"
     echo "Delete it if you wish to create a new one."
     exit 1
 fi
 
 
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
     echo "Defaulting image size to ${DEFAULT_IMAGE_SIZE_MB} MB."
     IMAGE_SIZE=${DEFAULT_IMAGE_SIZE}
 else
-    IMAGE_SIZE=$(($1 * 1024 * 1024))
+    IMAGE_SIZE=$(($2 * 1024 * 1024))
 fi
 
-${ANDROID_SDK_TOOLS}mksdcard $IMAGE_SIZE sdcard.img
+$ANDROID_HOME/tools/mksdcard $IMAGE_SIZE $1
