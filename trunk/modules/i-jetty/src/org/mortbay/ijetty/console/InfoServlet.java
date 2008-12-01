bf6ae24346d5235afe3c560ecf24733b22b28a49
aquabeta <aquabeta@d7dbba05-0943-0410-a12d-11b33697fbca>
2008-12-01 04:25:03 +0000

    * Put in place JSON and Javascript hooks for contact browser. Doesn't quite do anything useful yet. * Also removed iframe-based layout code. (for the most part)

--- trunk/modules/i-jetty/src/org/mortbay/ijetty/console/InfoServlet.java
+++ trunk/modules/i-jetty/src/org/mortbay/ijetty/console/InfoServlet.java
@@ -41,14 +41,31 @@ public abstract class InfoServlet extends HttpServlet
     {
         return (ContentResolver)getServletContext().getAttribute("contentResolver");
     }
+    
     protected void doHeader (PrintWriter writer,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doHeader(writer, request, response, null);
+    }
+    
+    protected void doHeader (PrintWriter writer,HttpServletRequest request, HttpServletResponse response, String[] scripts) throws ServletException, IOException
     {
         writer.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
         writer.println("<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>");
         writer.println("<head>");
         writer.println("    <title>i-jetty Console</title>");
         writer.println("    <link rel='stylesheet' type='text/css' media='screen' href='/console/console.css' />");
+        /* FIXME: This should be fixed before commit. */
+        writer.println("    <link rel='stylesheet' href='http://tablesorter.com/themes/blue/style.css' type='text/css' media='print, projection, screen' />");
         writer.println("    <meta name='viewport' content='width=device-width,minimum-scale=1.0,maximum-scale=1.0'/>");
+        
+        if (scripts != null)
+        {
+            for (String script : scripts)
+            {
+                writer.println ("    <script src=\"" + script + "\"></script>");
+            }
+        }
+        
         writer.println("</head>");
         writer.println("<body>");
     }
