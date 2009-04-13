5fff49c54f045616c9f48cdf107daf16e22e3442
aquabeta <aquabeta@d7dbba05-0943-0410-a12d-11b33697fbca>
2009-04-13 11:37:32 +0000

    * Lots'a HTML styling fixes. * Fix CSS a bit * Support browsers without console.log (eg IE)

--- trunk/modules/console/src/main/webapp/contacts.js
+++ trunk/modules/console/src/main/webapp/contacts.js
@@ -53,38 +53,39 @@ var User =
         {
             var uri =  "/console/contacts/json/";
             console.log("uri = "+uri);
-            $.ajax(
-                   {
-                       type: 'GET',
-                       url: uri,
-                       dataType: 'json',
-                       beforeSend: function(xhr)
-                       {
-                           xhr.setRequestHeader('Connection', 'Keep-Alive');
-                           return true;
-                       },
-                       success: function(response) 
-                       { 
-                           if (!successfn)
-                           {
-                               console.log("Got user list");
-                               User.users = response.users;
-                               User.version = response.version;
-                               User.renderUsers();
-                           }
-                           else
-                               successfn(response);
-                       },
-                       error: function(xhr, reason, exception) 
-                       { 
-                           if (!errfn)
-                           {
-                               console.log("Get Users Request failed, status: "+(xhr && xhr.status)+" reason: "+reason+" exception: "+ exception); 
-                           }
-                           else
-                               errfn (xhr, reason, exception);
-                       }
-                   });
+            $.ajax({
+                type: 'GET',
+                url: uri,
+                dataType: 'json',
+                beforeSend: function(xhr)
+                {
+                    xhr.setRequestHeader('Connection', 'Keep-Alive');
+                    return true;
+                },
+                success: function(response) 
+                { 
+                    if (!successfn)
+                    {
+                        console.log("Got user list");
+                        User.users = response.users;
+                        User.version = response.version;
+                        User.renderUsers();
+                    }
+                    else
+                        successfn(response);
+                },
+                error: function(xhr, reason, exception) 
+                { 
+                    if (!errfn)
+                    {
+                        console.log("Get Users Request failed, status: "+(xhr && xhr.status)+" reason: "+reason+" exception: "+ exception); 
+                    }
+                    else
+                    {
+                        errfn (xhr, reason, exception);
+                    }
+                }
+            });
             return false; 
         },
         
@@ -92,40 +93,39 @@ var User =
         {
             var uri = $(this).attr('href');
             console.log("uri = "+uri);
-            $.ajax(
-                   {
-                       type: 'GET',
-                       url: uri,
-                       dataType: 'json',
-                       beforeSend: function(xhr)
-                       {
-                           xhr.setRequestHeader('Connection', 'Keep-Alive');
-                           return true;
-                       },
-                       success: function(response) 
-                       { 
-                           if (!response.summary) 
-                           {
-                               alert ('Server returned bad data.');
-                               return;
-                           }
+            $.ajax({
+                type: 'GET',
+                url: uri,
+                dataType: 'json',
+                beforeSend: function(xhr)
+                {
+                    xhr.setRequestHeader('Connection', 'Keep-Alive');
+                    return true;
+                },
+                success: function(response) 
+                { 
+                    if (!response.summary) 
+                    {
+                        alert ('Server returned bad data.');
+                        return;
+                    }
 
-                           User.summary = response.summary;
+                    User.summary = response.summary;
 
-                           if (response.phones) 
-                               User.phones = response.phones;
+                    if (response.phones) 
+                        User.phones = response.phones;
 
-                           if (response.contacts)
-                               User.addresses = response.contacts;
+                    if (response.contacts)
+                        User.addresses = response.contacts;
 
-                           User.version = response.version;
-                           User.renderUser();
-                       },
-                       error: function(xhr, reason, exception) 
-                       { 
-                           console.log("Get User Request failed, status: "+(xhr && xhr.status)+" reason: "+reason+" exception: "+ exception); 
-                       }
-                   });
+                    User.version = response.version;
+                    User.renderUser();
+                },
+                error: function(xhr, reason, exception) 
+                { 
+                    console.log("Get User Request failed, status: "+(xhr && xhr.status)+" reason: "+reason+" exception: "+ exception); 
+                }
+            });
             return false;
         },
         
@@ -149,6 +149,25 @@ var User =
                 User.version = response.version; 
                 User.users = response.users; 
                 User.renderUsers();
+            }
+        },
+        
+        detectEnvironment: function ()
+        {
+            var agent = navigator.userAgent;
+            if (agent.indexOf("Android") > 0) {
+                User.mobileBrowser = true;
+            } else if (agent.indexOf("iPhone") > 0) {
+                User.mobileBrowser = true;
+            }
+            
+            if (User.mobileBrowser)
+            {
+                $("#userinfo").attr("style", "float: none;");
+            }
+            else if ($("#userinfo").attr("style") != null)
+            {
+                $("#userinfo").removeAttr("style");
             }
         },
         
@@ -246,20 +265,20 @@ var User =
             var html = "<div id='userinfo'>";
             html += "<form id='uform' method='POST' action='/console/contacts/json/?action="+User.action_save+"' enctype='multipart/form-data' target='hidden-target' onSubmit='User.saveUser(); return true;'>";
             html += "<input type='hidden' name='id' value='" + User.summary.id + "'>";
-            html += "<table><tr><td colspan='3'><h2>General</h2></td></tr>";
-            html += "<tr><td>Name: </td><td colspan='2' ><input name='name' type='text' value='" + User.summary.name + "' /></td></tr>";
-            html += "<tr><td>Starred: </td><td colspan='2' ><input name='starred' type='checkbox' " + (User.summary.starred ? "checked='checked'" : "") + " /></td></tr>";
-            html += "<tr><td>Send to Voicemail: </td><td colspan='2'><input name='voicemail' type='checkbox' "+(User.summary.voicemail? "checked='checked'" : "") + " /><td></tr>";
-            html += "<tr><td>Notes: </td><td colspan='2'  ><textarea name='notes'>" + (User.summary.notes != null ? User.summary.notes : "") + "</textarea></td></tr>";
-            html += "<tr><td colspan='2'><a href='/console/contacts/json/"+User.summary.id+"/photo'><img src='/console/contacts/json/"+User.summary.id+"/photo' /></a></td><td><input type='file' name='new-pic'>Change photo</input></td>";
-            html += "<tr><td colspan='3'><h2>Phone numbers</h2></td></tr>";
+            html += "<table><tr><td colspan='2'><h2>General</h2></td></tr>";
+            html += "<tr><td>Name: </td><td><input name='name' type='text' value='" + User.summary.name + "' /></td></tr>";
+            html += "<tr><td>Starred: </td><td ><input name='starred' type='checkbox' " + (User.summary.starred ? "checked='checked'" : "") + " /></td></tr>";
+            html += "<tr><td>Send to Voicemail: </td><td><input name='voicemail' type='checkbox' "+(User.summary.voicemail? "checked='checked'" : "") + " /></td></tr>";
+            html += "<tr><td>Notes: </td><td ><textarea name='notes'>" + (User.summary.notes != null ? User.summary.notes : "") + "</textarea></td></tr>";
+            html += "<tr><td><a href='/console/contacts/json/"+User.summary.id+"/photo'><img src='/console/contacts/json/"+User.summary.id+"/photo' /></a></td><td>Update photo:<br /><input type='file' name='new-pic' /></td></tr>";
+            html += "<tr><td colspan='2'><h2>Phone numbers</h2></td></tr>";
             for (var p in User.phones)
             {
                 html += User.renderPhone(User.phones[p]);
             }
             //add an extra blank for adding new number
             html += User.renderPhone ({number: "", id: "x", label: "", type: ""});
-            html += "<tr><td colspan='3'><h2>Addresses</h2></td></tr>";
+            html += "<tr><td colspan='2'><h2>Addresses</h2></td></tr>";
             for (var a in User.addresses)
             {
                 html += User.renderAddress(User.addresses[a]);
@@ -271,8 +290,8 @@ var User =
             html += "<br /><input type='submit' id='save' value='Save'/> ";  
             
             if (User.summary.id)
-                html += "<button id='del' onclick='User.deleteUser(); return false;'>Delete</button>";
-            html += "<button id='cancel' onclick='User.cancelEdit(); return false;'>Cancel</button>";
+                html += "<button id='del' onclick='User.deleteUser(); return false;'>Delete</button>&nbsp;";
+            html += "<button id='cancel' onclick='User.cancelEdit(); return false;'>Cancel</button>&nbsp;";
             html += "</form>";
             var content = $("#content");
             //if a mobile browser, we want to change the div content,
@@ -288,31 +307,26 @@ var User =
 
         renderPhone: function (phone)
         {
-            var html = "<tr>"; 
-            if (phone.id != "x")
-                html += "<td><input type='checkbox' name='phone-del-"+phone.id+"' value='del'>Delete</input></td>";  
-            else
-                html += "<td>&nbsp;</td>";
-            
+            var html = "<tr><td>"; 
             var selected = " selected='selected'";
-            html += "<td><select name='phone-type-" + phone.id + "'>";
+            html += "<select name='phone-type-" + phone.id + "'>";
             for (var l in User.labels)
             {     
                 html += "<option value='" + l + "'" + (phone.type == l ? selected : "") + ">" + User.labels[l] + "</option>";
             }   
             html += "</select></td>";     
-            html += "<td><input type='text' name='phone-number-" + phone.id + "' id='phone-number-" + phone.id + "' style='width: 120px;' length='12' value='" + phone.number + "' /></td></tr>";
+            html += "<td><input type='text' name='phone-number-" + phone.id + "' id='phone-number-" + phone.id + "' style='width: 120px;' length='12' value='" + phone.number + "' />";
+            if (phone.id != "x")
+                html += "&nbsp;<input type='checkbox' name='phone-del-"+phone.id+"' value='del'>Delete</input>";  
+            
+            html += "</td></tr>";
             return html;
         },
         
 
         renderAddress: function (address)
         {
-            var html = "<tr>";
-            if ("x" != address.id)
-                html += "<td><input type='checkbox' name='contact-del-"+address.id+"' value='del'>Delete</input></td>";
-            else
-                html += "<td>&nbsp;</td>";
+            var html = "<tr><td>";
             
             var kindSelect = "<select name='contact-kind-"+address.id+"'>";
             for (var k in User.kinds)
@@ -328,8 +342,11 @@ var User =
             }
             typeSelect += "</select>";
         
-            html +="<td>"+kindSelect+typeSelect+"</td><td><input type='text' name='contact-val-"+address.id+"' style='width: 120px;' length='12' value='"+address.data+"'/></td>";
-            html +="</tr>";
+            html +=kindSelect+typeSelect+"</td><td><input type='text' name='contact-val-"+address.id+"' style='width: 120px;' length='12' value='"+address.data+"'/>";
+            if ("x" != address.id)
+                html += "&nbsp;<input type='checkbox' name='contact-del-"+address.id+"' value='del'>Delete</input>";
+            
+            html +="</td></tr>";
             return html;
         },
        
@@ -368,17 +385,19 @@ var User =
   
 };
 
-    
+
+// For browsers without logging.
+if (console == null && document.console == null) {
+    document.console = {
+        log: function() {
+            return;
+        }
+    };
+}
 
 
-$(document).ready (
-                   function () 
-                   {
-                       var agent = navigator.userAgent;
-                       if (agent.indexOf("Android") > 0)
-                           User.mobileBrowser = true;
- 
-                       User.getUsers();
-                       console.log("document ready");
-                   }); 
-
+$(document).ready (function () {
+    User.detectEnvironment();
+    User.getUsers();
+    console.log("document ready");
+}); 
