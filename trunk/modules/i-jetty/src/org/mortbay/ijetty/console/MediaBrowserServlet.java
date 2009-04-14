62cdb3080eddd0c1aeb3284a346a43150cf79264
aquabeta <aquabeta@d7dbba05-0943-0410-a12d-11b33697fbca>
2009-04-14 07:42:35 +0000

    Finish up image support for media browser servlet.

--- trunk/modules/i-jetty/src/org/mortbay/ijetty/console/MediaBrowserServlet.java
+++ trunk/modules/i-jetty/src/org/mortbay/ijetty/console/MediaBrowserServlet.java
@@ -17,8 +17,13 @@ package org.mortbay.ijetty.console;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -27,16 +32,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.mortbay.ijetty.console.HTMLHelper;
+import org.mortbay.util.IO;
 
 import android.provider.BaseColumns;
 import android.provider.MediaStore;
 import android.net.Uri;
 import android.database.Cursor;
 import android.content.ContentResolver;
+import android.graphics.Bitmap;
 
 public class MediaBrowserServlet extends HttpServlet
 {
-    private Uri[] __MEDIA_URIS = { MediaStore.Images.Media.EXTERNAL_CONTENT_URI };
+    private Uri[] __MEDIA_URIS = { MediaStore.Images.Media.EXTERNAL_CONTENT_URI, MediaStore.Images.Media.INTERNAL_CONTENT_URI };
     
     private ContentResolver resolver;
 
@@ -53,38 +60,89 @@ public class MediaBrowserServlet extends HttpServlet
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            // If nothing was passed (including ending /)
+            // just do default stuff
+            doBody(request, response);
+            return;
+        }
+        
+        String typestr = null;
+        String item = null;
+        
+        StringTokenizer strtok = new StringTokenizer(pathInfo, "/");
+        if (strtok.hasMoreElements())          
+            typestr = strtok.nextToken();
+        
+        if (strtok.hasMoreElements())          
+            item = strtok.nextToken();
+        
+        if (typestr == null || item == null) {
+            // Either type and item weren't found (both required)
+            doBody(request, response);
+            return;
+        }
+        
+        try {
+            Long rowid = Long.parseLong (item);
+            int type = Integer.parseInt (typestr);
+            
+            Uri contenturi = __MEDIA_URIS[type];
+            Uri content = Uri.withAppendedPath(contenturi, item);
+            
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(resolver, content);
+            if (bitmap != null) {
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+                ByteArrayInputStream stream = new ByteArrayInputStream(bytes.toByteArray());
+                
+                response.setContentType("image/png");
+                response.setStatus(HttpServletResponse.SC_OK);
+                OutputStream os = response.getOutputStream();
+                IO.copy(stream,os);
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        
+    }
+
+    protected void doBody(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
         PrintWriter writer = response.getWriter();
         HTMLHelper.doHeader(writer, request, response);
         HTMLHelper.doMenuBar(writer, request, response);
-        doContent(writer, request, response);
-        HTMLHelper.doFooter (writer, request, response);
-    }
-
-    protected void doContent(PrintWriter writer, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        writer.println("<h1 class='pageheader'>Media</h1><div id='content'>");
         
         List <Uri> media = new ArrayList <Uri> ();
-        
+    
+        writer.println("<h1 class='pageheader'>Media</h1><div id='content'>");
         writer.println("<ul>");
         
+        Integer type = 0;
         int count = 0;
         
         for (Uri contenturi : __MEDIA_URIS)
         {
             Cursor cur = resolver.query (contenturi, null, null, null, null);
-            cur.moveToFirst();
+            //cur.moveToFirst();
             while (cur.moveToNext())
             {
                 Long rowid = cur.getLong (cur.getColumnIndexOrThrow(BaseColumns._ID));
                 String name = cur.getString (cur.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME));
                 
                 media.add (Uri.withAppendedPath(contenturi, rowid.toString()));
-                writer.println("<li>" + name + "</li>");
+                writer.println("<li><a href='/console/media/" + type.toString() + "/" + rowid.toString() + "'>" + name + "</a></li>");
                 count++;
             }
+            
+            type++;
         }
         
         if (count == 0)
@@ -92,6 +150,7 @@ public class MediaBrowserServlet extends HttpServlet
         
         writer.println("</ul>");
         writer.println("</div>");
+        
+        HTMLHelper.doFooter (writer, request, response);
     }
-
 }
