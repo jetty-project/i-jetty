1f72fb32ac6a8ac8efc19cf487a7edf354c53c10
joakim.erdfelt <joakim.erdfelt@d7dbba05-0943-0410-a12d-11b33697fbca>
2010-04-06 00:04:08 +0000

    Updating deploy.sh

--- trunk/i-jetty-ui/deploy.sh
+++ trunk/i-jetty-ui/deploy.sh
@@ -11,16 +11,21 @@ if [ -z $DEVICEID ] ; then
 fi
 
 echo ""
-echo "Uninstalling any existing ijetty ..."
-adb -s $DEVICEID uninstall org.mortbay.ijetty
+echo "Looking for installed ijetty ..."
+adb -s $DEVICEID shell pm list packages | grep ijetty
+if [ $? == 0 ] ; then
+  echo "Uninstalling existing ijetty ..."
+  adb -s $DEVICEID uninstall org.mortbay.ijetty
+fi
 
 echo ""
 echo "Installing new ijetty ..."
-adb -s $DEVICEID install target/i-jetty-*.apk
+APKFILE=`ls -1 target/i-jetty-*.apk | head -1`
+adb -s $DEVICEID install $APKFILE
 
 echo ""
 echo "Starting ijetty ..."
 adb -s $DEVICEID shell am start -a android.intent.action.MAIN \
  -c android.intent.category.LAUNCHER \
- -n org.mortbay.ijetty.IJetty
+ -n org.mortbay.ijetty/.IJetty
 
